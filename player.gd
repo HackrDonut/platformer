5dcb358ed66b5534a317469139c4e3694ceb7778
HackrDonut <luke.a.pruitt@gmail.com>
2023-10-18 11:14:51 -0500

    End of Day 2
    Added player and basic movement

---
+++ player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var is_jumping = false
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		is_jumping = false
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	update_animation(direction)
+	move_and_slide()
+
+func update_animation(direction):
+	if is_jumping:
+		animated_sprite_2d.play("jump")
+	elif direction != 0:
+		animated_sprite_2d.flip_h = (direction < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
