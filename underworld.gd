e755d5c271894cadfe7f86949cc6ee16cc16d869
HackrDonut <luke.a.pruitt@gmail.com>
2023-10-26 11:13:24 -0500

    Video 9 End (New Level)
    The player can now beat the first level and transition into a second level.

--- underworld.gd
+++ underworld.gd
@@ -10,12 +10,3 @@ func _ready():
 func _process(delta):
 	pass
 
-
-func _on_interact_body_entered(body):
-	if body.is_in_group("Player"):
-		body.die()
-
-
-func _on_transition_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://underworld.tscn")
